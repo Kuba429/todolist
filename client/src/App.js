584814b2cf6ce54d940acf228c39d78fc6322fa1
Kuba429 <jakubzawadzki429@gmail.com>
2021-02-20 13:56:47 +0100

    Added login page (front end)

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,19 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
 
+//compoents
+import LoginPage from './components/LoginPage';
+import GlobalStyle from './GlobalStyle';
 
 function App() {
-  return (
-    <div className="App">
-     
-    </div>
-  );
+    const [logged, setLogged] = useState(false);
+
+    return (
+        <div className="App">
+            <GlobalStyle />
+            {!logged && <LoginPage />}
+        </div>
+    );
 }
 
 export default App;
---
+++ client/src/GlobalStyle.js
@@ -0,0 +1,19 @@
+import {createGlobalStyle} from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+    *{
+        padding:0px;
+        margin:0px;
+        box-sizing:border-box;
+    }
+
+
+
+
+
+
+
+`
+
+
+export default GlobalStyle;
